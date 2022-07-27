225997cf358138443ad7eed1755c4206ec42c36e
HakureiAnna <hakurei.anna@gmail.com>
2022-07-27 15:00:14 +0900

    correct dbserver script errors

--- caldown/dbserver/scripts/02_insertData.sql
+++ caldown/dbserver/scripts/02_insertData.sql
@@ -1,10 +1,8 @@
-
-
 INSERT INTO users(id, username, password, status)
 VALUES
 (UUID(), 'testuser', (SHA2(CONCAT('password'), 256)), 0);
 
-INSERT INTO profile(id, age, weight, height, gender, userid) 
+INSERT INTO profiles(id, age, weight, height, gender, userid) 
 SELECT
 	UUID(), 20, 50, 165, 1, id
 FROM users 
