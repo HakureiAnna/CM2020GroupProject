7aee075b90285c58ea02c32174b03c9846e053c2
HakureiAnna <hakurei.anna@gmail.com>
2022-08-09 15:45:27 +0900

    logic addition to unit tests for /history

--- caldown/backend/tests/unit/test_history.py
+++ caldown/backend/tests/unit/test_history.py
@@ -109,17 +109,47 @@ def test_history_valid_data():
         json=data
     ).json()
 
-    url = 'https://localhost/api/history'
+    url = 'https://localhost/api/plan'
+    d = date.today()
+    d += timedelta(days=2)
+    d = d.strftime('%Y/%m/%d')
     headers = {
         'Authorization': 'Bearer ' + retVal['token']
     }
+    data = {
+        'breakfast': {
+            'name': 'chicken chops',
+            'uri': 'https://www.google.com',
+            'image': 'https://yahoo.com',
+            'calories': 1000
+        },
+        'lunch': {
+            'name': 'chicken chops',
+            'uri': 'https://www.google.com',
+            'image': 'https://yahoo.com',
+            'calories': 1000
+        },
+        'dinner': {
+            'name': 'chicken chops',
+            'uri': 'https://www.google.com',
+            'image': 'https://yahoo.com',
+            'calories': 1000
+        },
+        'plannedDate': d
+    }
+    retVal = requests.post(url,
+        verify=False,
+        headers=headers,
+        json=data).json()
+
+    url = 'https://localhost/api/history'    
     d = date.today()
     d1 = d - timedelta(days=7)
     d2 = d + timedelta(days=7)
     d1 = d1.strftime('%Y/%m/%d')
     d2 = d2.strftime('%Y/%m/%d')
     params = 'startDate=' + d1 + '&' + 'endDate=' + d2
-    retVal = requests.post(url + '?' + params,
+    retVal = requests.get(url + '?' + params,
         verify=False,
         headers=headers).json()
     assert len(retVal['plans']) > 0
