70d92f57de4fb1f438c654730ba5b91763343848
Queen Latifa <latifa@Latifas-MacBook-Air.local>
2022-08-12 20:58:32 +0200

    questions appear onclick

--- caldown/frontend/app/src/components/Questions.vue
+++ caldown/frontend/app/src/components/Questions.vue
@@ -23,19 +23,31 @@ function handleSubmit(event) {
     console.log(err);
   });
 }
+
+function show(){
+  document.getElementById('getStarted').style.cssText = "display: none";
+  // document.getElementById('form-age').style.cssText = "display: block";
+  // document.getElementById('form-weight').style.cssText = "display: block";
+  let questions = document.getElementsByClassName('questions');
+  for(var i=0; i<questions.length; i++){
+    questions[i].style.cssText = "display: block";
+  }
+}
+
 </script>
 
 <template>
   <div class="container sm-col-6">
-    <div class="row m-3">
+    <div class="title row m-3">
       <h2>Let's get started!</h2>
     </div>
-    <div class="row ms-3 mb-3">
-      We're going to use the following questions to help determine your
-      recommended total daily caloric intake.
+    <div class="welcome row m-3">
+      <p>We're going to use the following questions to help determine your
+      recommended total daily caloric intake.</p>
+      <button @click = "show" type="submit" id = "getStarted" class="btn btn-primary">Let's Go!</button>
     </div>
     <form @submit.prevent="handleSubmit">
-      <div class="form-floating mb-3 form-group">
+      <div class="questions form-floating mb-3 form-group">
         <input
           type="number"
           class="form-control"
@@ -44,7 +56,7 @@ function handleSubmit(event) {
         />
         <label for="floatingInput">Age</label>
       </div>
-      <div class="form-floating mb-3 form-group">
+      <div class="questions form-floating mb-3 form-group">
         <input
           type="number"
           class="form-control"
@@ -53,8 +65,9 @@ function handleSubmit(event) {
         />
         <label for="floatingPassword">Weight (in kg)</label>
       </div>
+
       <select
-        class="form-select mb-3 form-group"
+        class="questions form-select mb-3 form-group"
         aria-label="Gender"
         v-model="gender"
       >
@@ -63,7 +76,7 @@ function handleSubmit(event) {
         <option value="2">Female</option>
       </select>
       <select
-        class="form-select mb-3 form-group"
+        class="questions form-select mb-3 form-group"
         aria-label="Weekly Physical Activity"
         v-model="wpa"
       >
@@ -73,7 +86,7 @@ function handleSubmit(event) {
         <option value="3">Heavy (more than 3 times a week)</option>
       </select>
       <select
-        class="form-select mb-3 form-group"
+        class="questions form-select mb-3 form-group"
         aria-label="Nutritional Goal"
         v-model="ng"
       >
@@ -82,10 +95,42 @@ function handleSubmit(event) {
         <option value="2">Weight maintenance</option>
         <option value="3">Weight gain (bulking)</option>
       </select>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="questions btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
 
-<style></style>
-3
+
+<style>
+.btn {
+  background-color: #e68a35;
+}
+
+.btn:hover{
+  background-color: #e68a35; 
+}
+
+.title{
+  height: 8vh;
+  position: relative;
+}
+.title h2{
+  justify-content: center;
+  display: flex;
+  align-items: center;
+}
+
+.welcome{
+  padding: none;
+  position: relative;
+  justify-content: center
+}
+.welcome p{
+  text-align: center;
+}
+
+.questions{
+  display: none;
+}
+
+</style>
